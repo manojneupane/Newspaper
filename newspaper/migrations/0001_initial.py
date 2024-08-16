ff643adc63a3b7bd088cbdb582b38f4a979b032f
Manoj Kumar Neupane <manojkneupane@gmail.com>
2024-08-16 21:33:39 +0545

    newspaper design second day

--- newspaper/migrations/0001_initial.py
+++ newspaper/migrations/0001_initial.py
@@ -15,47 +15,90 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('name', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("name", models.CharField(max_length=100)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Tag',
+            name="Tag",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('name', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("name", models.CharField(max_length=100)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Post',
+            name="Post",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('title', models.CharField(max_length=200)),
-                ('content', models.TextField()),
-                ('featured_image', models.ImageField(upload_to='post_images/%Y/%m/%d')),
-                ('status', models.CharField(choices=[('active', 'Actuve'), ('in_active', 'Inactive')], default='active', max_length=20)),
-                ('view_count', models.PositiveIntegerField(default=0)),
-                ('published_at', models.DateTimeField(blank=True, null=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='newspaper.category')),
-                ('tag', models.ManyToManyField(to='newspaper.tag')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("title", models.CharField(max_length=200)),
+                ("content", models.TextField()),
+                ("featured_image", models.ImageField(upload_to="post_images/%Y/%m/%d")),
+                (
+                    "status",
+                    models.CharField(
+                        choices=[("active", "Actuve"), ("in_active", "Inactive")],
+                        default="active",
+                        max_length=20,
+                    ),
+                ),
+                ("view_count", models.PositiveIntegerField(default=0)),
+                ("published_at", models.DateTimeField(blank=True, null=True)),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "category",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="newspaper.category",
+                    ),
+                ),
+                ("tag", models.ManyToManyField(to="newspaper.tag")),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from newspaper.models import Post
+
+from django.views.generic import ListView
+
+
+class HomeView(ListView):
+    model=Post
+    template_name="aznews/home.html"
+    context_object_name="posts"
+    
