bdf9e517790a989500d064d9b281755baa98777f
Manoj Kumar Neupane <manojkneupane@gmail.com>
2024-08-18 18:30:50 +0545

    Newspaper Trending Add

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,44 +1,65 @@
 from django.db import models
 
-# Create your models here.
 
 class timeStampModel(models.Model):
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract=True #don't create table in Database
+        abstract = True  # don't create table in Database
 
-        
 
 class Category(timeStampModel):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+
 class Tag(timeStampModel):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+
 class Post(timeStampModel):
-    sTATUS_CHOICES=[
+    sTATUS_CHOICES = [
         ("active", "Actuve"),
         ("in_active", "Inactive"),
     ]
-    title=models.CharField(max_length=200)
-    content=models.TextField()
-    featured_image=models.ImageField(upload_to="post_images/%Y/%m/%d", blank=False )
-    author=models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    status=models.CharField(max_length=20, choices=sTATUS_CHOICES, default="active")
-    view_count=models.PositiveIntegerField(default=0)
-    published_at=models.DateTimeField(null=True, blank=True)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
-    tag=models.ManyToManyField(Tag)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to="post_images/%Y/%m/%d", blank=False)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=sTATUS_CHOICES, default="active")
+    view_count = models.PositiveIntegerField(default=0)
+    published_at = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
-    
 
+
+## 1 - 1 Relationship
+# 1 user can have 1 profile => 1
+# 1 profile is associated to 1 user  => 1
+# OneToOneField() => Can be used in Any Model
+
+
+## 1 - M Relationship
+# 1 user can post M post  => M
+# 1 post is associated to only 1 user => 1
+# In django use ForeignKey() => Must be used in Many side
+
+# 1 category can have M posts => M
+# 1 post is associated to only 1 category => 1
+
+## M - M Relationship
+# 1 student can learn from M teachers => M
+# 1 teacher can teach M students => M
+# ManyToManyField() => Can be used in Any Model
+
+# 1 post can contain M tag => M
+# 1 tag can be used in M post => M
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
 from newspaper.models import Post
 
 from django.views.generic import ListView
 
 
 class HomeView(ListView):
-    model=Post
-    template_name="aznews/home.html"
-    context_object_name="posts"
-    
+    model = Post
+    template_name = "aznews/home.html"
+    context_object_name = "posts"
+    queryset = Post.objects.filter(published_at__isnull=False, status="active")[:5]
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+
+    #     context["featured_post"] = (
+    #         Post.objects.filter(published_at__isnull=False, status="active")
+    #         .order_by("-published_at", "-views_count")
+    #         .first()
+    #     )
+    #     context["featured_posts"] = Post.objects.filter(
+    #         published_at__isnull=False, status="active"
+    #     ).order_by("-published_at", "-views_count")[1:4]
+
+    #     return context
